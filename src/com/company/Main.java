fd45c90c7a273805656d2d01e9c754eacb9379f3
BuildTools <unconfigured@null.spigotmc.org>
2018-08-25 22:48:01 -0400

    Finished project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int random = (int )(Math.random() * 100 + 1);
-        System.out.println("Try to guess the secret number! hint: its this lol: " + random);
+        System.out.println("Try to guess the secret number!");
         //gets the random number
 
 
