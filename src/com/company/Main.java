180a08acc1865e0160a029e4a0b435cae15e8ff1
BuildTools <unconfigured@null.spigotmc.org>
2018-08-25 22:44:42 -0400

    Finished project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+
+        int random = (int )(Math.random() * 100 + 1);
+        System.out.println("Try to guess the secret number! hint: its this lol: " + random);
+        //gets the random number
+
+
+
+        while(true){
+
+            int guess = 0;
+            Scanner keyboard = new Scanner(System.in);
+            guess = keyboard.nextInt();
+            //user inputs guess
+
+
+            if (guess == random) {
+                System.out.println("You guessed the number!");
+                System.exit(0);
+            }
+            else if (guess > random) {
+                System.out.println("The secret number is lower than your guess!");
+            }
+            else {
+                System.out.println("The secret number is higher than your guess!");
+            }
+        }
     }
 }
